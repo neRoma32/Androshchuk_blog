66a85f97cf1a6069c03228149fa6cc2f04f55735
arararara9 <roman2005viktor@gmail.com>
2025-06-17 22:13:23 +0300

    лабораторна 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +16,9 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
+
+Route::resource('rest', RestTestController::class)->names('restTest');
